27a60b0a208ccf3de39f88a93e1eef685fe44109
Coursera Learner <nobody@nowhere.nul>
2019-10-26 16:27:17 +0000

    WIP
    Keep using Ctrl-W and Ctrl-N in xterm.js and it is driving me crazy.  Unfortunately, squares_test.o doesn't have the source and it doesn't link correctly on my local box.  Going to guess what it is doing and try to run it locally

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -24,12 +24,21 @@ int isAtBorder(int coord, int offset, int size) {
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
+  int w = size1;
+  if ((x_offset + size2) > size1) {
+    w = x_offset + size2;
+  }
 
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int h = size1;
+  if ((y_offset + size2) > size1) {
+    h = y_offset + size2;
+  }
+  
   //count from 0 to h. Call the number you count with y
-
+  for (int y = 0; y < h; y++) {
     //count from 0 to w. Call the number you count with x
+    for (int x = 0; x < w; x++) {
 
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
@@ -38,16 +47,33 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-
+      if (
+	  (((x_offset < x) && (x < (x_offset + size2))) && ((y == y_offset) || (y == (y_offset + size2 - 1))))
+	  ||
+	  (((y_offset < y) && (y < (y_offset + size2))) && ((x == x_offset) || (x == (x_offset + size2 -1))))
+	 ) {
+	printf("*");
+      }
       //if not,
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
-
-      //else print a space
+      else if (
+	       ((x < size1) && ((y == 0) || (y == (size1 - 1))))
+	       ||
+	       ((y < size1) && ((y == 0) || (x == (size1 - 1))))
+	       ) {
+	printf("#");
+      }
+      else {
+	//else print a space
+	printf(" ");
+      }
+    }
     //when you finish counting x from 0 to w, 
     //print a newline
-
+    printf("\n");
+  }
 }
